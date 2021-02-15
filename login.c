933fa0c21acd7547beee984aa42b5b77a105a9f2
Anil <kumarchowdary467@gmail.com>
2021-02-15 23:31:08 +0000

    third commiut

--- login.c
+++ login.c
@@ -11,7 +11,7 @@ ruct User {
    kk onst char* name;
     nst char* password;
 };
-const struct User VALID_USERS[NR_USERS] = {"john", "long", "demo", "mode"};
+const stct User VALID_USERS[NR_USERS] = {"john", "long", "demo", "mode"};
 
 
 int validate_user(const char* name, const char* password) {
